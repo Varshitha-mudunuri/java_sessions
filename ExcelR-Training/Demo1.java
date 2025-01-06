4286fe53ac82938574df0d2d21b1261f49aa3942
Varshitha-mudunuri <151758265+Varshitha-mudunuri@users.noreply.github.com>
2025-01-06 09:32:01 +0530

    Add files via upload
    Conditional Statements

---
+++ ExcelR-Training/Demo1.java
@@ -0,0 +1,13 @@
+public class session_4 {
+    public static void main(String[] args){
+        int x=5,y=3;
+        if(x>y){
+            System.out.println("x is greater");
+        }
+        else{
+            System.out.println("y is greater");
+        }
+    }
+   
+
+}
